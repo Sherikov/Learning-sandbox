48b0aa0d6fe336a139da933a17979439b47f9759
Sherikov Nafasbek <sherikov966@gmail.com>
2026-01-31 18:40:01 +0500

    Fix

--- Landing-page-collection/Robot-school/src/js/script.js
+++ Landing-page-collection/Robot-school/src/js/script.js
@@ -1,10 +1,10 @@
 const track = document.querySelector('.carousel-track');
 
-// 1. Берем все карточки, которые есть сейчас
+// 1. take all the cards that we have now.
 const cards = Array.from(track.children);
 
-// 2. Клонируем каждую карточку и добавляем в конец ленты
-// Теперь у нас будет набор: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+// 2. Clone each card and add it to the end of the feed
+// Now we'll have a set: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
 cards.forEach(card => {
     const clone = card.cloneNode(true);
     track.appendChild(clone);
